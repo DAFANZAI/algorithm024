36665234f966fb4d53de546640e06cef1a0e2022
dfz <347888927@qq.com>
2021-01-31 17:31:41 +0800

    第一周作业提交

---
+++ Week_01/code/MoveZeros_283.java
@@ -0,0 +1,43 @@
+public class MoveZeros_283 {
+    /**
+     * 暴力解法：第一层循环数组中每个元素；第二层循环把数组元素向前移动至前一个为非零元素的位置
+     * 复杂度：O(n^2)
+     * @param nums
+     */
+    public void moveZeroes1(int[] nums) {
+        for(int i = 1; i <nums.length; i++) {
+            if(nums[i] != 0) {
+                for(int j = i - 1; j >= 0; j--) {
+                    if(nums[j] == 0) {
+                        nums[j] = nums[j + 1];
+                        nums[j + 1] = 0;
+                    }else {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * 双指针法：定义两个指针，一个指针遍历数组，一个指针查找第一个0元素
+     * 复杂度：O(n)
+     * @param nums
+     */
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                if(i != j) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+                j++;
+            }
+        }
+        for(; j < nums.length; j++) {
+            nums[j] = 0;
+        }
+    }
+}
---
+++ Week_01/code/TwoSum_1.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum_1 {
+    /**
+     * 暴力解法：两次遍历数组查找相加等于目标值的两个元素
+     * 复杂度：O(n^2)
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum1(int[] nums, int target) {
+        int[] result = new int[2];
+        for(int i = 0; i < nums.length - 1; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * HashMap解法：使用空间换时间
+     * 复杂度：O(n)
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int[] result = new int[2];
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(target - nums[i])) {
+                result[0] = map.get(target - nums[i]);
+                result[1] = i;
+                return result;
+            }
+            map.put(nums[i], i);
+        }
+        return result;
+    }
+}
